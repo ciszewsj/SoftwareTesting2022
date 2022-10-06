83b248d83ac2a80c80909bf7c060fb08d6886242
jciszewski <jciszewski@amusys.pl>
2022-10-06 13:03:10 +0200

    add main controller

---
+++ src/main/java/ee/informatyka/softwaretesting2022wut/controller/MainController.java
@@ -0,0 +1,15 @@
+package ee.informatyka.softwaretesting2022wut.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class MainController {
+
+	@GetMapping
+	public String getMainView() {
+		return "main";
+	}
+}
