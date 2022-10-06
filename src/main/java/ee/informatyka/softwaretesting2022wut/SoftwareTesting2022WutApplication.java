9e4ff0aa954e556c40ee34cc73d829aded2dfd86
jciszewski <jciszewski@amusys.pl>
2022-10-06 12:42:23 +0200

    first commit

---
+++ src/main/java/ee/informatyka/softwaretesting2022wut/SoftwareTesting2022WutApplication.java
@@ -0,0 +1,13 @@
+package ee.informatyka.softwaretesting2022wut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTesting2022WutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareTesting2022WutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/informatyka/softwaretesting2022wut/SoftwareTesting2022WutApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.informatyka.softwaretesting2022wut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTesting2022WutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
