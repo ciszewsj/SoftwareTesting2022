a5594e7f303e3f85fdf76153151d0f6b9a631ae5
jciszewski <jciszewski@amusys.pl>
2022-10-06 20:51:14 +0200

    add validation

--- src/main/java/ee/informatyka/softwaretesting2022wut/data/User.java
+++ src/main/java/ee/informatyka/softwaretesting2022wut/data/User.java
@@ -2,6 +2,7 @@ package ee.informatyka.softwaretesting2022wut.data;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "USERS")
@@ -11,8 +12,10 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Size(min = 6, max = 35, message = "Username should be 6-35 chars")
 	public String username;
 
+	@Size(min = 6, max = 35, message = "Password should be 6-35 chars")
 	public String password;
 
 	@Email(message = "It's not an email")
