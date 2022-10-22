852ac1339c1c104629d59a4b362ee57fe8b068ac
jciszewski <jciszewski@amusys.pl>
2022-10-22 18:59:44 +0200

    rename

--- src/main/java/com/example/metodywytwarzaniaoprogramowania/Shop.java
+++ src/main/java/com/example/metodywytwarzaniaoprogramowania/Shop.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MetodyWytwarzaniaOprogramowaniaApplication {
+public class Shop {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MetodyWytwarzaniaOprogramowaniaApplication.class, args);
+		SpringApplication.run(Shop.class, args);
 	}
 
 }
