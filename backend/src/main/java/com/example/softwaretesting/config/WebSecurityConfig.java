935fb2c6e7feb86276dfa229cebe1a9fbde4bdf8
jciszewski <jciszewski@amusys.pl>
2022-11-29 22:40:24 +0100

    add cors

--- backend/src/main/java/com/example/softwaretesting/config/WebSecurityConfig.java
+++ backend/src/main/java/com/example/softwaretesting/config/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable()
+		httpSecurity.cors().and().csrf().disable()
 				.headers().frameOptions().disable().and()
 				.authorizeRequests()
 				.antMatchers("/h2-console/*").permitAll()
